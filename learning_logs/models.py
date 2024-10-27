da9c6e4fa37401b8dc9b33f2556c9865875b46fd
Aditya Prasad <adityap.is22@bmsce.ac.in>
2024-10-27 22:44:14 +0530

    Created superuser using "python manage.py createsuperuser". Registered our model 'Topic' with the admin site Added Topics 'Chess and Rock Climbing' using the admin site Defined the 'Entry Model' Migrated the Entry Model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -9,3 +9,23 @@ class Topic(models.Model):
     def __str__(self):
         """Return a string representation of the model"""
         return self.text
+
+# Defining the Entry Model
+class Entry(models.Model):
+    """Something specific learned about a topic"""
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    """ on_delete=models.CASCADE means that if the related Topic instance is deleted, 
+    all related Entry instances will also be deleted."""
+
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """set a special attribute telling Django to use
+        'Entries' when it needs to refer to more than one entry (Otherwise would have been Entrys"""
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text[:50] + "..."
